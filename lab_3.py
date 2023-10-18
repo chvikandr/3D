c19095f4b6c258b45d9a04b82f6b1cb18cc34606
chvikandr <102212226+chvikandr@users.noreply.github.com>
2023-10-18 20:17:52 +0300

    Add files via upload

---
+++ lab_3.py
@@ -0,0 +1,152 @@
+import math
+
+from PIL import Image
+
+def line (x0, y0, x1, y1):
+    if x0 > x1:
+        a = x0
+        x0 = x1
+        x1 = a
+        b = y0
+        y0 = y1
+        y1 = b
+    if x1 == x0:
+        if y0>y1:
+            b = y0
+            y0 = y1
+            y1 = b
+        for i in range(y0, y1):
+            image.putpixel((x1, i), (0, 0, 255))
+    else:
+        k = abs((y1 - y0) / (x1 - x0))
+        e = 0
+        if k <= 1:
+            y = y0
+            if y1 >= y0:
+                for x in range(x0, (x1 + 1)):
+                    if e > (x1 - x0):
+                        y += 1
+                        e -= 2 * (x1 - x0)
+                    e += 2 * (y1 - y0)
+                    image.putpixel((x, y), (0, 0, 255))
+            else:
+                for x in range(x0, (x1 + 1)):
+                    if e > (x1 - x0):
+                        y -= 1
+                        e -= 2 * (x1 - x0)
+                    e += 2 * (y0 - y1)
+                    image.putpixel((x, y), (0, 0, 255))
+        else:
+            x = x0
+            if y1 >= y0:
+                for y in range(y0, (y1 + 1)):
+                    if e > (y1 - y0):
+                        x += 1
+                        e += 2 * (y1 - y0)
+                    e += 2 * (x1 - x0)
+                    image.putpixel((x, y), (0, 0, 255))
+            else:
+                for y in range(y0, (y1 + 1), -1):
+                    if e > (y0 - y1):
+                        x += 1
+                        e -= 2 * (y0 - y1)
+                    e += 2 * (x1 - x0)
+                    image.putpixel((x, y), (0, 0, 255))
+
+def check(xn, yn, x1, y1):
+    return xn * x1 + yn * y1 == 0
+
+
+image = Image.new('RGB', (100,100))
+print("Введите количество вершин выпуклого многоугольника")
+n=int(input())
+print("Введите последовательно вершины многоугольника.Вершины вводятся по часовой стрелке.Координаты Х и Y через enter")
+verx=[None]*n
+very=[None]*n
+for i in range(0, n):
+    print("Введите вершину")
+    verx[i]=int(input())
+    very[i]=int(input())
+print("Вевдите отрезок, который должен отсечь многоугольник")
+print("Введите координаты первой точки")
+xa=int(input())
+ya=int(input())
+print("Введите координаты второй точки")
+xb=int(input())
+yb=int(input())
+kolmin = 0
+tmin = []
+tmax = []
+for i in range(0, n):
+    if i == n-1:
+        x0 = verx[i]
+        y0 = very[i]
+        x1 = verx[0]
+        y1 = very[0]
+    else:
+        x0 = verx[i]
+        y0 = very[i]
+        x1 = verx[i+1]
+        y1 = very[i+1]
+    if (x0 == x1):
+        if y1 > y0:
+            xn = -1
+        else:
+            xn = 1
+        yn = 0
+        if check (xn, yn, (xb - xa), (yb - ya)):
+            pass
+        else:
+            t = (x0 - xa) / (xb - xa)
+            if xn * (xb - xa) + yn * (yb - ya) > 0:
+                tmax.append(t)
+            else:
+                tmin.append(t)
+                kolmin += 1
+    elif (y0 == y1):
+        if x1 > x0:
+            yn = 1
+        else:
+            yn = -1
+        xn = 0
+        if check (xn, yn, (xb - xa), (yb - ya)):
+            pass
+        else:
+            t = (y0 - ya) / (yb - ya)
+            if xn * (xb - xa) + yn * (yb - ya) > 0:
+                tmax.append(t)
+            else:
+                tmin.append(t)
+                kolmin += 1
+    else:
+        if ((x1 > x0) and (y1 > y0)) or ((x1 < x0) and (y1 < y0)):
+            xn = x0 - x1
+            yn = y1 - y0
+        elif ((x1 > x0) and (y1 < y0)) or ((x1 < x0) and (y1 > y0)):
+            xn = x1 - x0
+            yn = y0 - y1
+        if check (xn, yn, (xb - xa), (yb - ya)):
+            pass
+        else:
+            t = (xn * (x0 - xa) + yn * (y0 - ya)) / (xn * (xb - xa) + yn * (yb - ya))
+            if xn * (xb - xa) + yn * (yb - ya) > 0:
+                tmax.append(t)
+            else:
+                tmin.append(t)
+                kolmin += 1
+    line(x0, y0, x1, y1)
+tmin.sort()
+tmax.sort()
+if (xa <= xb) and (ya <= yb):
+    line(xa, ya, xa + math.ceil((xb - xa) * tmin[kolmin-1]), ya + math.ceil((yb - ya) * tmin[kolmin-1]))
+    line(xb, yb, xa + math.ceil((xb - xa) * tmax[0]), ya + math.ceil((yb - ya) * tmax[0]))
+elif (xa >= xb) and (ya <= yb):
+    line(xa, ya, xa - math.ceil((xa-xb) * tmin[kolmin-1]), ya + math.ceil((yb-ya)*tmin[kolmin-1]))
+    line(xb, yb, xa - math.ceil((xa-xb) * tmax[0]), ya + math.ceil((yb-ya)*tmax[0]))
+elif (xa <= xb) and (ya >= yb):
+    line(xa, ya, xa + math.ceil((xb - xa) * tmin[kolmin-1]), ya - math.ceil((ya - yb) * tmin[kolmin-1]))
+    line(xb, yb, xa + math.ceil((xb - xa) * tmax[0]), ya - math.ceil((ya - yb) * tmax[0]))
+elif (xa >= xb) and (ya >= yb):
+    line(xa, ya, xa - math.ceil((xa-xb) * tmin[kolmin-1]), ya - math.ceil((ya - yb) * tmin[kolmin-1]))
+    line(xb, yb, xa - math.ceil((xa-xb) * tmax[0]), ya - math.ceil((ya - yb) * tmax[0]))
+image.save("a.png")
