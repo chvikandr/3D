18811ce064bd09b5e2333ed1e07d3198a59168f2
chvikandr <102212226+chvikandr@users.noreply.github.com>
2023-12-21 22:59:21 +0300

    lab_4.py

--- lab_4.py
+++ lab_4.py
@@ -1,33 +1,30 @@
 from PIL import Image
 import matplotlib.pyplot as plt
-from math import sqrt
 
 
-def Gauss(x0, y0, width, height, pix_vals):
-    rgb = [0] * 3
-    for i in range(3):
-        for X in range(x0 - 1, x0 + 1 + 1):
-            for Y in range(y0 - 1, y0 + 1 + 1):
-                if (X < 0 or Y < 0 or X > width or Y > height): continue
-                k = 1
-                if (X == x0 and Y == y0):
-                    k *= 4
-                elif (X == x0 or Y == y0):
-                    k *= 2
-                rgb[i] += k * pix_vals[width * Y + X][i]
-        rgb[i] = int(rgb[i] / 16)
-        rgb[i] = 255 if rgb[i] > 255 else rgb[i]
-    return tuple([int(sum(rgb) / 3)] * 3)
-
-
-with Image.open("valve.png") as image:
+with Image.open("a.png") as image:
     new_image = Image.new('RGB', (image.width, image.height))
-    pixel_values = list(image.getdata())
+    pixel = list(image.getdata())
     for x in range(0, image.width - 1):
         for y in range(0, image.height - 1):
-            new_color = Gauss(x, y, image.width, image.height, pixel_values)
+            rgb = [0] * 3
+            for i in range(3):
+                for X in range(x - 1, x + 1 + 1):
+                    for Y in range(y - 1, y + 1 + 1):
+                        if X < 0 or Y < 0 or X > image.width or Y > image.height:
+                            continue
+                        k = 1
+                        if X == x and Y == y:
+                            k *= 4
+                        elif X == x or Y == y:
+                            k *= 2
+                        rgb[i] += k * pixel[image.width * Y + X][i]
+                rgb[i] = int(rgb[i] / 16)
+                if rgb[i] > 255:
+                    rgb[i] = 255
+            new_color = tuple([int(sum(rgb) / 3)] * 3)
             new_image.putpixel((x, y), new_color)
-    new_image.save("new_valve.png")
+    new_image.save("b.png")
     plt.imshow(image)
     plt.show()
     plt.imshow(new_image)
